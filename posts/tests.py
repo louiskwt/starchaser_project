4ddadeb332cfedec2fc4e6ea83c4617f92e188e3
Louis Tsang <kawingt2@gmail.com>
2022-08-07 10:22:07 +0800

    wrote some new tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,15 +1,23 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
 from django.urls import reverse
+from .models import Post
+from django.contrib.auth.models import User
 
-class HomepageTests(SimpleTestCase):
+class BlogPostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.published_posts.create(title='Test Title', body='Test Body', status='published', author=User.objects.create(username='testuser'))
+
+    def test_model_content(self):
+        self.assertEqual(self.post.title, 'Test Title')
+        self.assertEqual(self.post.body, 'Test Body')
+        self.assertEqual(self.post.status, 'published')
+        self.assertEqual(self.post.author.username, 'testuser')
+    
     def test_url_exists_at_correct_location(self):
-        response = self.client.get('/')
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-    
-    def test_url_available_by_name(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
-    
+
     def test_template_name_correct(self):
         response = self.client.get(reverse('home'))
         self.assertTemplateUsed(response, 'posts/home.html')
