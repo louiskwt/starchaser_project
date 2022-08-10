a176fec216be26225840adc8683c2441c576c137
Louis Tsang <kawingt2@gmail.com>
2022-08-11 00:07:56 +0800

    built detail blog page and fix nav home link

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,6 @@ from .views import post_list, post_detail
 app_name = 'posts' # This is used to namespace the urls.
 
 urlpatterns = [
-    path("", post_list, name="post_list"),
+    path("", post_list, name="home"),
     path("<int:year>/<int:month>/<int:day>/<slug:post>/", post_detail, name="post_detail"),
 ]
