8bfe747910c9b619a1999eed518990797ec09f86
Louis Tsang <kawingt2@gmail.com>
2022-08-14 23:34:35 +0800

    test app

--- posts/tests.py
+++ posts/tests.py
@@ -6,18 +6,19 @@ from django.contrib.auth.models import User
 class BlogPostTests(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.post = Post.published_posts.create(title='Test Title', body='Test Body', status='published', author=User.objects.create(username='testuser'))
+        cls.post = Post.published_posts.create(title='Test Title', body='Test Body', status='published', tags='react', author=User.objects.create(username='testuser'))
 
     def test_model_content(self):
         self.assertEqual(self.post.title, 'Test Title')
         self.assertEqual(self.post.body, 'Test Body')
         self.assertEqual(self.post.status, 'published')
         self.assertEqual(self.post.author.username, 'testuser')
+        self.assertEqual(self.post.tags, 'react')
     
     def test_url_exists_at_correct_location(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
     def test_template_name_correct(self):
-        response = self.client.get(reverse('home'))
+        response = self.client.get(reverse('posts:home'))
         self.assertTemplateUsed(response, 'posts/home.html')
