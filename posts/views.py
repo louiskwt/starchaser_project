429ff9543e5eda92c09c60dd0366e1b8a3c5e168
Louis Tsang <kawingt2@gmail.com>
2022-08-14 10:06:08 +0800

    rebuild post_list and home page

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Post
+from taggit import models as taggit_models
 
 # class HomePageView(TemplateView):
 #     posts = Post.published_posts.all()
 #     template_name: str = 'posts/home.html'
 #     extra_context = { 'posts': posts }
 
+def home_display(request, tag_slug=None):
+    tags = taggit_models.Tag.objects.all()
+    context = {
+        'tags': tags,
+    }
+    return render(request, 'posts/home.html', context)
+
 def post_list(request, tag_slug=None):
-    posts = Post.published_posts.all()
-    return render(request, 'posts/home.html', {'posts': posts})
+    object_list = Post.published_posts.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(taggit_models.Tag, slug=tag_slug)
+        posts = object_list.filter(tags__in=[tag])
+    else:
+        posts = object_list
+    
+    context = {
+        'posts': posts,
+        'tag': tag,
+    }
+
+    return render(request, 'posts/post_list.html', context)
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
