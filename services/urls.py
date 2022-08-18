1c9d30dee9f271dfde833f7ea6d9660d743d5cac
Louis Tsang <kawingt2@gmail.com>
2022-08-18 21:44:44 +0800

    built service page

--- services/urls.py
+++ services/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import about_view
+from .views import about_view, services_view
 
 app_name = 'services' # This is used to namespace the urls.
 
 urlpatterns = [
-    path("about", about_view, name="about")
+    path("about", about_view, name="about"),
+    path("services", services_view, name="services")
 ]
--- services/views.py
+++ services/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def about_view(request):
     return render(request, 'about/about.html')
 
+def services_view(request):
+    return render(request, 'services/services.html')
+
