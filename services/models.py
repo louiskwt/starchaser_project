3da9408c2854e180e8e59cb18f53dbb55a3a61da
Louis Tsang <kawingt2@gmail.com>
2022-08-19 23:30:07 +0800

    set up model and updated setting for services

--- services/models.py
+++ services/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class SiteContent(models.Model):
+    name = models.CharField(max_length=100)
+    body = models.TextField()
+    links = models.JSONField(null=True)
+
+    def __str__(self):
+        return self.name
--- starchaser/settings.py
+++ starchaser/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'posts.apps.PostsConfig',
     'taggit',
+    'services.apps.ServicesConfig'
 ]
 
 MIDDLEWARE = [
