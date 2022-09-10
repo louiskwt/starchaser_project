bd5b9ef1ed37b720fd044b28789a4a37da10f979
Louis Tsang <kawingt2@gmail.com>
2022-09-10 16:10:59 +0800

    set up member model and form

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,27 @@
+from dataclasses import fields
 from django import forms
+from .models import Member
 
 # Login Form
 class LoginForm(forms.Form):
-    form_template_name = "login_snippet.html"
-    username = forms.CharField()
-    password = form.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(label="用户名")
+    password = forms.CharField(widget=forms.PasswordInput, label="密碼")
+
+class UserRegistrationForm(forms.Modelform):
+    password = forms.CharField(label="密碼", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="確認密碼", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("2次輸入的密碼碼一樣，請再次輸入")
+        return cd['password']
+
+class MemberFrom(forms.ModelForm):
+    class Meta:
+        model = Member
+        fields('phone_num', 'date_of_birth', 'gender', 'role', 'status', 'descritpion')
--- account/urls.py
+++ account/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     # post views
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', views.user_login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
