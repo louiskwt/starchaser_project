9081ba6fff96e350cf6811e88a1e61e696790ccf
Louis Tsang <kawingt2@gmail.com>
2022-09-18 21:06:36 +0800

    Update model to include price for member
    add price column to DB and migrate

--- account/models.py
+++ account/models.py
@@ -65,7 +65,9 @@ class Member(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     date_of_birth = models.DateField(blank=True, null=True)
+
     phone_num = models.CharField(max_length=8, blank=True)
+
     gender = models.CharField(max_length=7, choices=GENDER_CHOICES, default=EMPTY, null=False)
 
     role = models.CharField(max_length=7, choices=ROLE_CHOICES, default=EMPTY, null=False)
@@ -77,6 +79,8 @@ class Member(models.Model):
     active = models.CharField(max_length=3, choices=YES_NO_CHOICES, default=YES, null=False)
 
     subject = models.CharField(max_length=8, choices=SUBJECTS_CHOICES, default=EMPTY, null=False)
+
+    price = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return f'Profile for user {self.user.username}'
