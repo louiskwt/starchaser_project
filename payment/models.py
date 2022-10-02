22599eead931fff31dcb814d7e4799ab0cc1b043
Louis Tsang <kawingt2@gmail.com>
2022-10-02 23:19:37 +0800

    Add payment record model
    add payment record model and migrate

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,15 @@
+from email.policy import default
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class PaymentRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    payment_date = models.DateField().auto_now
+    name = models.CharField(max_length=56, default="", null=False)
+    email = models.CharField(max_length=156, default="", null=False)
+
+
+    def __str__(self):
+        return f'Payment record for {self.user.username}'
+
