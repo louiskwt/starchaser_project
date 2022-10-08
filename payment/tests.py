2ca0315d90a045385f57be2d707acba1c2a681e7
Louis Tsang <kawingt2@gmail.com>
2022-10-08 23:31:33 +0800

    wrote simple test

--- payment/tests.py
+++ payment/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class UrlTest(TestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get('/payment/')
+        self.assertEqual(response.status_code, 200)
