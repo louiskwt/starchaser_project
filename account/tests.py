5b7ecb9131f4729ba41f10cfe6954e05f794344d
Louis Tsang <kawingt2@gmail.com>
2022-10-13 23:55:59 +0800

    modify test

--- account/tests.py
+++ account/tests.py
@@ -9,7 +9,7 @@ class UserSignUpTests(TestCase):
 
     def test_signup_page_status_code(self):
         response = self.client.get("/member/register")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 301)
     
     def test_view_url_by_name(self):
         response = self.client.get(reverse("register"))
