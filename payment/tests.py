5c7ce9b30fc9595f96c23a3230c3ecee551bf2e6
Louis Tsang <kawingt2@gmail.com>
2022-10-15 22:58:45 +0800

    test payment template

--- payment/tests.py
+++ payment/tests.py
@@ -6,3 +6,7 @@ class UrlTest(TestCase):
     def test_url_exists_at_correct_location(self):
         response = self.client.get('/payment/')
         self.assertEqual(response.status_code, 200)
+    
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('payment'))
+        self.assertTemplateUsed(response, "payment/payment.html")
