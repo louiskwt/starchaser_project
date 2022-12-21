77d5d215f6057f6c838183e49496bd21f1329900
Louis Tsang <kawingt2@gmail.com>
2022-12-21 18:43:38 +0800

    improved animation

--- js_toolchain/tailwind.config.js
+++ js_toolchain/tailwind.config.js
@@ -7,16 +7,27 @@ module.exports = {
         "fade-in-down": {
           "0%": {
             opacity: "0",
-            transform: "translateY(-10px)",
+            transform: "translateY(-20px)",
           },
           "100%": {
             opacity: "1",
             transform: "translateY(0)",
           },
         },
+        "fade-in-right": {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(50px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0)",
+          },
+        },
       },
       animation: {
-        "fade-in-down": "fade-in-down 1.0s ease-out",
+        "fade-in-down": "fade-in-down 1.5s ease-out",
+        "fade-in-right": "fade-in-right 1.0s ease-out",
       },
     },
   },
